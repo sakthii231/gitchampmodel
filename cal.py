8f89989cd9f34f4d2ccd9fd734e0e219c24a7ec9
sakthii231 <shakthii2309876@gmail.com>
2025-09-10 12:08:14 +0530

    Upload

---
+++ cal.py
@@ -0,0 +1,14 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    pass
+def mul(a,b):
+    pass
+
+
+
+a=int(input("Enter a No 1."))
+b=int(input("Enter a No.2"))
+print(add(a,b))
+print(sub(a,b))
+print(mul(a,b))
